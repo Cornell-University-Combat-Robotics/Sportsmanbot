5cb01706a472741783bfede31112a9a99ecd78d5
Ashrafur Siddiqui <siddiqua@bxscience.edu>
2021-02-27 14:20:04 -0500

    Added modes to the respective pins

--- boundary/boundary.ino
+++ boundary/boundary.ino
@@ -66,6 +66,14 @@ void MoveReverse(int dist, int motspeed)
 
 void setup() {
   Serial.begin(9600);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
   // delay?
 }
 
