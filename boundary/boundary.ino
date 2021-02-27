125d803bbb7b2eed613504503bef79ed4e8e8b35
Rob Measner <rjm464@cornell.edu>
2021-02-27 15:11:48 -0500

    better structure

--- boundary/boundary.ino
+++ boundary/boundary.ino
@@ -1,118 +1,103 @@
-/*#include Ultrasonic.h
+//pin for h-bridge motor controler
+#define IN1 2
+#define IN2 3
+#define IN3 4
+#define IN4 7
+#define ENA 5
+#define ENB 6
 
-Ultrasonic ultrasonic( /* location of sensor? */ );
-const int IN1 = /*pin number*/;
-const int IN2 = /*pin number*/;
-const int IN3 = /*pin number*/;
-const int IN4 = /*pin number*/;
-#define IR_sensor_front A0 // front sensor
-#define IR_sensor_back A1 // rear senson
-int distance ;
+//pin sensor
+#include<NewPing.h>
+#define TRIGGER_PIN 8
+#define ECHO_PIN    9
+#define MAX_DISTANCE 50000
 
-const byte Motor_A = 3;
-const byte Motor_B = 2;
+unsigned long previousMillis = 0;
+const long interval = 1000;
 
-// Motor A
-int ENA = 10;
-int IN1 = 9;
-int IN2 = 8;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
-// Motor B
-int ENB = 5;
-int IN3 = 6;
-int IN4 = 7;
-
-// Function to Move Forward
-void MoveForward(int dist, int motspeed) 
-{
-   // Set Motor A forward
-   digitalWrite(IN1, HIGH);
-   digitalWrite(IN2, LOW);
- 
-   // Set Motor B forward
-   digitalWrite(IN3, HIGH);
-   digitalWrite(IN4, LOW);
-   
-   // Go forward until some point
-   //  while () {
-   //  }
-      
-   // Stop when done
-   analogWrite(ENA, 0);
-   analogWrite(ENB, 0);
-}
- 
-// Function to Move in Reverse
-void MoveReverse(int dist, int motspeed) 
-{  
-  // Set Motor A reverse
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
- 
-  // Set Motor B reverse
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-   
-   // Go in reverse until some point
-   // while () {
-   // 
-   // }
-    
-   // Stop when done
-   analogWrite(ENA, 0);
-   analogWrite(ENB, 0);
-}
-
+//int for random turn direction
+int ranturn = 0;
+long r1 = 0;
+long r2 = 500;
 
 void setup() {
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  
   Serial.begin(9600);
-  // delay?
+
 }
 
 void loop() {
 
-  int IR_front = analogRead(IR_sensor_front);
-  int IR_back = analogRead(IR_sensor_back);
-  distance = ultrasonic.read();
+  //int IR_front = analogRead(IR_sensor_front);
+  //int IR_back = analogRead(IR_sensor_back);
   
-  // add turning? until spot other robot
-
-  if (IR_front /* compare to boundary line color */) {
-    // delays ?
-    Stop();
-    BACKWARD(255);
-  } 
-   
-  if (IR_back /* compare to boundary line color */ ) {
-    Stop();
-    FORWARD(255);
-  } 
+  unsigned long currentMillis = millis();   //time for no delay
+  unsigned long randomDirection = millis(); //timer for setting random turn direction
+  unsigned int uS = sonar.ping();           //check distance
+  
+if(randomDirection-r1 > r2 && ranturn==0){  //check to see if the corect time has passed
+  ranturn = ranturn+1;                      //if time has passed and ranturn ==0 then set ranturn to 1
+  r1 = randomDirection;
 }
-
-void FORWARD (int Speed) {
-  analogWrite(IN1,Speed);
-  analogWrite(IN2,0);
-  analogWrite(IN3,0);
-  analogWrite(IN4,Speed);
+else if(randomDirection-r1 > r2 && ranturn==1){
+  ranturn = ranturn-1; //if ranturn is == to 1 set it to 0
+  r1 = randomDirection;
 }
+  //Serial.print(ranturn); 
 
-void BACKWARD (int Speed) {
-  analogWrite(IN1,0);
-  analogWrite(IN2,Speed);
-  analogWrite(IN3,Speed);
-  analogWrite(IN4,0);
-}
+  if (uS  >= 3000) //if every thing is clear go forward
+  {
+    forword(50);
+  }
+  else if (uS < 3000 && ranturn==0) //if something is there turn left.
+  {
+    stay();
+    delay(500);
+    backword(90);
+    delay(1000);
+    left(100);
+    delay(500);
+    unsigned int uS2 = sonar.ping();
+    if (uS2 < 2000){
+      left(100);
+      delay(500);
+      unsigned int uS2 = sonar.ping();
+      if (uS2 < 2000){
+        left(100);
+        delay(500);
+      }
+    } 
+  }
+  else if (uS < 3000 && ranturn==1){
+    stay();
+    delay(500);
+    backword(75);
+    delay(500);
+    right(100);
+    delay(500);
+    unsigned int uS2 = sonar.ping();
+    if (uS2 < 2000){
+      right(100);
+      delay(500);
+      unsigned int uS2 = sonar.ping();
+      if (uS2 < 2000){
+        right(100);
+        delay(500);
+      }
+    }
+  }
+  Serial.print("uS");
+  Serial.print(uS);
+  Serial.println();
+  
 
-void ROTATE (int Speed) {
-  analogWrite(IN1,Speed);
-  analogWrite(IN2,0);
-  analogWrite(IN3,Speed);
-  analogWrite(IN4,0);
-}
-
-void Stop() {
-  analogWrite(IN1,0);
-  analogWrite(IN2,0);
-  analogWrite(IN3,0);
-  analogWrite(IN4,0);
 }
---
+++ boundary/moveCases.ino
@@ -0,0 +1,87 @@
+//void setup for the right motor
+void rm(int mode, int percent)
+{
+  //change the percentage range of 0 -> 100 into the PWM
+  //range of 0 -> 255 using the map function
+  int movespeed = map(percent, 0, 100, 0, 255);
+
+  switch(mode)
+  {
+    case 0: //stop right motor
+    digitalWrite(ENA, LOW);
+    break;
+
+    case 1: // right motor forword
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, movespeed);
+    break;
+
+    case 2: // right motor bacwords
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, movespeed);
+    break;
+    }
+
+}
+//end of void rm
+
+
+//void setup for left motor
+void lm(int mode, int percent)
+{
+  //change the percentage range of 0 -> 100 into the PWM
+  //range of 0 -> 255 using the map function
+  int movespeed = map(percent, 0, 100, 0, 255);
+  
+  switch(mode)
+  {
+    case 0: //stop left motor
+    digitalWrite(ENB, LOW);  //set enable low to disable B
+    break;
+
+    case 1: //left motor forword
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, movespeed);
+    break;
+
+    case 2: //left motor backwords
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENB, movespeed);
+    break;
+  }
+}
+//end of void lm
+
+
+void forword (int movespeed) //used to move robot forword. movespeed sets the speed.
+{
+  rm(1,movespeed);
+  lm(1,movespeed);
+}
+
+void backword (int movespeed) //used to move robot backwords
+{
+  rm(2,movespeed);
+  lm(2,movespeed);
+}
+
+void left (int movespeed) //turn robot left
+{
+  rm(1,movespeed);
+  lm(2,movespeed);
+}
+void right(int movespeed) //turn robot right
+{
+  rm(2,movespeed);
+  lm(1,movespeed);
+}
+
+void stay () //stop robot
+{
+  rm(0,0);
+  lm(0,0);
+}
