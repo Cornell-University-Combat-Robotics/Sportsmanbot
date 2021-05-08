e079f437a30eab25b3e20d043c75e65a68d4688d
Rob Measner <rjm464@cornell.edu>
2021-05-08 19:00:15 -0400

    fixed overwrite issue; tests working now

--- boundary/moveCasesTest.cpp
+++ boundary/moveCasesTest.cpp
@@ -43,11 +43,160 @@ void fake_print(std::string s)
   return;
 }
 
+void moveInit(struct movement* m, int len, int lca, int lcb, int ren, int rca, int rcb, bool t = true)
+{
+  m->currentMovement = still;
+  m->lEnable = len;
+  m->lControlA = lca;
+  m->lControlB = lcb;
+  m->lmF = false;
+  m->lmB = false;
+  m->lSpeed = 0;
+  m->rEnable = ren;
+  m->rControlA = rca;
+  m->rControlB = rcb;
+  m->rmF = false;
+  m->rmB = false;
+  m->rSpeed = 0;
+}
+
+/*
+ * Sets up the right motor
+ * @param mode defines whether motor is off (0), moves
+ * forward (1) or backward (2)
+ * @param percent is the speed of the motor from 0-100%
+ */
+void rm(struct movement* m, int mode, int percent)
+{
+  //change the percentage range of 0 -> 100 into the PWM
+  //range of 0 -> 255 using the map function
+  int movespeed = map(percent, 0, 100, 0, 255);
+
+  switch (mode)
+  {
+  case 0: //stop right motor
+    digitalWrite(m->lEnable, LOW);
+    break;
+
+  case 1: // right motor forword
+    digitalWrite(m->lControlA, LOW);
+    digitalWrite(m->lControlB, HIGH);
+    analogWrite(m->lEnable, movespeed);
+    break;
+
+  case 2: // right motor bacwords
+    digitalWrite(m->lControlA, HIGH);
+    digitalWrite(m->lControlB, LOW);
+    analogWrite(m->lEnable, movespeed);
+    break;
+  }
+}
+//end of void rm
+
+/*
+ * Sets up the left motor
+ * @param mode defines whether motor is off (0), moves
+ * forward (1) or backward (2)
+ * @param percent is the speed of the motor from 0-100%
+ */
+void lm(struct movement* m, int mode, int percent)
+{
+  //change the percentage range of 0 -> 100 into the PWM
+  //range of 0 -> 255 using the map function
+  int movespeed = map(percent, 0, 100, 0, 255);
+
+  switch (mode)
+  {
+  case 0:                   //stop left motor
+    digitalWrite(m->rEnable, LOW); //set enable low to disable B
+    break;
+
+  case 1: //left motor forword
+    digitalWrite(m->rControlA, HIGH);
+    digitalWrite(m->rControlB, LOW);
+    analogWrite(m->rEnable, movespeed);
+    break;
+
+  case 2: //left motor backwords
+    digitalWrite(m->rControlA, LOW);
+    digitalWrite(m->rControlB, HIGH);
+    analogWrite(m->rEnable, movespeed);
+    break;
+  }
+}
+//end of void lm
+
+void forward(struct movement* m, int movespeed, bool t = true) //used to move robot forword. movespeed sets the speed.
+{
+  rm(m, 1, movespeed);
+  lm(m, 1, movespeed);
+  m->currentMovement = mforward;
+  m->lmF = true;
+  m->lmB = false;
+  m->lSpeed = movespeed;
+  m->rmF = true;
+  m->rmB = false;
+  m->rSpeed = movespeed;
+}
+
+void backward(struct movement* m, int movespeed, bool t = true) //used to move robot backwords
+{
+  rm(m, 2, movespeed);
+  lm(m, 2, movespeed);
+  m->currentMovement = mbackward;
+  m->lmF = false;
+  m->lmB = true;
+  m->lSpeed = movespeed;
+  m->rmF = false;
+  m->rmB = true;
+  m->rSpeed = movespeed;
+}
+
+//TODO: Maybe add pivots? i.e. one motor off and the other backward/forward?
+
+void left(struct movement* m, int movespeed, bool t = true) //turn robot left
+{
+  rm(m, 1, movespeed);
+  lm(m, 2, movespeed);
+  m->currentMovement = mleft;
+  m->lmF = false;
+  m->lmB = true;
+  m->lSpeed = movespeed;
+  m->rmF = true;
+  m->rmB = false;
+  m->rSpeed = movespeed;
+}
+
+void right(struct movement* m, int movespeed, bool t = true) //turn robot right
+{
+  rm(m, 2, movespeed);
+  lm(m, 1, movespeed);
+  m->currentMovement = mright;
+  m->lmF = true;
+  m->lmB = false;
+  m->lSpeed = movespeed;
+  m->rmF = false;
+  m->rmB = true;
+  m->rSpeed = movespeed;
+}
+
+void stay(struct movement* m, bool t = true) //stop robot
+{
+  rm(m, 0, 0);
+  lm(m, 0, 0);
+  m->currentMovement = still;
+  m->lmF = false;
+  m->lmB = false;
+  m->lSpeed = 0;
+  m->rmF = false;
+  m->rmB = false;
+  m->rSpeed = 0;
+}
 
 int main()
 {
   struct movement test_mve;
-  moveInit(&test_mve, LEFT_E, LEFT_C1, LEFT_C2, RIGHT_E, RIGHT_C1, RIGHT_C2);
+  moveInit(&test_mve, LEFT_E, LEFT_C1, LEFT_C2, RIGHT_E, RIGHT_C1, RIGHT_C2, true);
 
   //init state
   assert(test_mve.currentMovement == still);
@@ -59,7 +208,7 @@ int main()
   assert(test_mve.rSpeed == 0);
 
   //move forward 50% speed
-  forward(&test_mve, 50);
+  forward(&test_mve, 50, true);
   assert(test_mve.currentMovement == mforward);
   assert(test_mve.lmF == true);
   assert(test_mve.lmB == false);
@@ -69,7 +218,7 @@ int main()
   assert(test_mve.rSpeed == 50);
 
   //move backward 50% speed
-  backward(&test_mve, 50);
+  backward(&test_mve, 50, true);
   assert(test_mve.currentMovement == mbackward);
   assert(test_mve.lmF == false);
   assert(test_mve.lmB == true);
@@ -79,7 +228,7 @@ int main()
   assert(test_mve.rSpeed == 50);
 
   //move left 50% speed
-  left(&test_mve, 50);
+  left(&test_mve, 50, true);
   assert(test_mve.currentMovement == mleft);
   assert(test_mve.lmF == false);
   assert(test_mve.lmB == true);
@@ -89,7 +238,7 @@ int main()
   assert(test_mve.rSpeed == 50);
 
   //move right 50% speed
-  left(&test_mve, 50);
+  right(&test_mve, 50, true);
   assert(test_mve.currentMovement == mright);
   assert(test_mve.lmF == true);
   assert(test_mve.lmB == false);
@@ -97,6 +246,16 @@ int main()
   assert(test_mve.rmF == false);
   assert(test_mve.rmB == true);
   assert(test_mve.rSpeed == 50);
+
+  //stop robot
+  stay(&test_mve, true);
+  assert(test_mve.currentMovement == still);
+  assert(test_mve.lmF == false);
+  assert(test_mve.lmB == false);
+  assert(test_mve.lSpeed == 0);
+  assert(test_mve.rmF == false);
+  assert(test_mve.rmB == false);
+  assert(test_mve.rSpeed == 0);
 
   return 0;
 }
